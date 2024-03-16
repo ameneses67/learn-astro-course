5a77f85afc6e66ba1c61d2cf1046c0b20aa60f41
Poncho <ameneses67@gmail.com>
2024-03-16 17:17:23 -0600

    Modulo 41-1

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
-}
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: "#00d6f7",
+					secondary: "#9b9900",
+
+					accent: "#976c00",
+
+					neutral: "#242823",
+
+					"base-100": "#262626",
+
+					info: "#009ef4",
+
+					success: "#00ac6a",
+
+					warning: "#ae3e00",
+
+					error: "#ff305a",
+				},
+			},
+		],
+	},
+};
