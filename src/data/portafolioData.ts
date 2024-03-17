fe7e51ba4c2a87075b56b96a07b866fdb349273e
Poncho <ameneses67@gmail.com>
2024-03-16 21:53:35 -0600

    Modulo 42-1

---
+++ src/data/portafolioData.ts
@@ -0,0 +1,33 @@
+export const baseData = {
+	title: "Portafolio de Karina Hernandez",
+	description:
+		"¡Bienvenido a mi portafolio! Soy una diseñadora y desarrolladora web autodidacta obsesionada con la calidad.",
+};
+
+export const socialData = [
+	{
+		title: "dribbble",
+		href: "https://dribbble.com/",
+		icon: "icon--dribbble",
+	},
+	{
+		title: "instagram",
+		href: "https://instagram.com/",
+		icon: "icon--instagram",
+	},
+	{
+		title: "twitter",
+		href: "https://twitter.com/",
+		icon: "icon--twitter",
+	},
+	{
+		title: "youtube",
+		href: "https://youtube.com/",
+		icon: "icon--youtube",
+	},
+	{
+		title: "github",
+		href: "https://github.com/",
+		icon: "icon--github",
+	},
+];
