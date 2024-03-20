d5ad998849b7d357b9750518297b2afc146de0b8
Poncho <ameneses67@gmail.com>
2024-03-20 13:27:28 -0600

    Modulos 59 y 60

--- src/content/config.ts
+++ src/content/config.ts
@@ -28,8 +28,28 @@ const projects = defineCollection({
 		}),
 });
 
+const posts = defineCollection({
+	type: "content",
+	schema: ({ image }) =>
+		z.object({
+			title: z.string().max(65, {
+				message: "El título no puede ser mayor de 65 caracteres de largo.",
+			}),
+			description: z.string().max(165, {
+				message:
+					"La descripción no puede ser mayor de 165 caracteres de largo.",
+			}),
+			image: image().refine((img) => img.width >= 1000, {
+				message: "La imagen debe medir por lo menos 1,000 pixeles de ancho.",
+			}),
+			pubDate: z.date(),
+			isDraft: z.boolean().optional(),
+		}),
+});
+
 export const collections = {
 	features,
 	tools,
 	projects,
+	posts,
 };
