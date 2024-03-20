105449defc71f2887728edb9e01dabba0dbbaad3
Poncho <ameneses67@gmail.com>
2024-03-19 20:47:35 -0600

    Module 56-1

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
+import type { CollectionEntry } from "astro:content";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
+}
+
+export function getSearchParamsAsArray(url: URL, key: string) {
+	const params = new URLSearchParams(url.search);
+	const value = params.get(key);
+
+	if (!value) {
+		return [];
+	}
+
+	return value.split(",") as CollectionEntry<"tools">["id"][];
 }
