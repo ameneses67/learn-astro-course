ffd8c170395d4bc11426fcde077b88b619179461
Poncho <ameneses67@gmail.com>
2024-03-19 21:46:23 -0600

    Module 57-1

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,3 +16,25 @@ export function getSearchParamsAsArray(url: URL, key: string) {
 
 	return value.split(",") as CollectionEntry<"tools">["id"][];
 }
+
+export function setNewUrlParams({
+	url,
+	key,
+	valueArray,
+}: {
+	url: URL;
+	key: string;
+	valueArray: string[];
+}) {
+	const params = new URLSearchParams(url.search);
+
+	if (valueArray.length === 0) {
+		params.delete(key);
+		url.search = params.toString();
+		return url;
+	}
+
+	params.set(key, valueArray.join(","));
+	url.search = params.toString();
+	return url;
+}
